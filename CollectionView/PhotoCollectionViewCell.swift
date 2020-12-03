a2086f22e1b6469b0d0cb933aebda0a3b6a408ed
vanh1200 <axitpicric@gmail.com>
2020-12-03 19:10:13 +0700

    update

---
+++ CollectionView/PhotoCollectionViewCell.swift
@@ -0,0 +1,138 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CollectionView
+//
+//  Created by Kieu Viet Anh on 03/12/2020.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    static let identifier = "PhotoCollectionViewCell"
+    
+    var url: String? {
+        didSet {
+            if let url = URL(string: self.url ?? "") {
+                imageView.load(url: url)
+            }
+        }
+    }
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private var gradient: CAGradientLayer?
+    private var label: UILabel?
+    private var view: UIView?
+    private var bg: GradientView?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        
+        let bg = GradientView()
+        contentView.addSubview(bg)
+        bg.translatesAutoresizingMaskIntoConstraints = false
+        bg.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        bg.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        bg.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        bg.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        let colorTop = UIColor(red: 0 / 255.0, green: 0 / 255.0, blue: 0 / 255.0, alpha: 0.3)
+        let colorBottom = UIColor(red: 0 / 255.0, green: 0 / 255.0, blue: 0 / 255.0, alpha: 0.9)
+        bg.topColor = colorTop
+        bg.bottomColor = colorBottom
+        
+        let images = [
+//            UIImage(named: "1"),
+//            UIImage(named: "2"),
+//            UIImage(named: "3"),
+//            UIImage(named: "4"),
+//            UIImage(named: "5"),
+//            UIImage(named: "6"),
+            UIImage(named: "7"),
+            UIImage(named: "8"),
+            UIImage(named: "9"),
+            UIImage(named: "10"),
+            UIImage(named: "11"),
+        ].compactMap({$0})
+        
+        imageView.image = images.randomElement()
+//        if let url = URL(string: self.url ?? "") {
+//            imageView.load(url: url)
+//        }
+        
+        label = UILabel()
+        contentView.addSubview(label!)
+        
+        label!.textAlignment = .center
+        label!.textColor = .white
+        label!.backgroundColor = UIColor.clear
+        label!.translatesAutoresizingMaskIntoConstraints = false
+        label!.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        label!.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        label!.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        label!.text = "Dummy"
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        url = ""
+    }
+}
+
+@IBDesignable class GradientView: UIView {
+    @IBInspectable var topColor: UIColor = UIColor.red {
+        didSet {
+            setGradient()
+        }
+    }
+    @IBInspectable var bottomColor: UIColor = UIColor.blue {
+        didSet {
+            setGradient()
+        }
+    }
+
+    override class var layerClass: AnyClass {
+        return CAGradientLayer.self
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setGradient()
+    }
+
+    private func setGradient() {
+        (layer as! CAGradientLayer).colors = [topColor.cgColor, bottomColor.cgColor]
+        (layer as! CAGradientLayer).startPoint = CGPoint(x: 0.5, y: 0.0)
+        (layer as! CAGradientLayer).endPoint = CGPoint(x: 0.5, y: 1.0)
+        (layer as! CAGradientLayer).locations = [0.0, 1.0]
+    }
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async {[weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -12,12 +12,94 @@ class ViewController: UIViewController {
     private let collectionView = UICollectionView(
         frame: .zero,
         collectionViewLayout: UICollectionViewFlowLayout())
+    
+    private var images = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        initImages()
+        collectionView.register(PhotoCollectionViewCell.self, forCellWithReuseIdentifier: PhotoCollectionViewCell.identifier)
         collectionView.delegate = self
         collectionView.dataSource = self
         view.addSubview(collectionView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+    
+    func initImages() {
+        images.append("https://cf.shopee.vn/file/0254aea1b7d9cc4c95435563a4e5fa89")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-30.jpg?v=1586766112957")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-3495.jpg?v=1593490441000")
+        images.append("https://aodaitrunghieu.com/wp-content/uploads/2019/07/Vai-ao-dai-hoa-3D-tren-nguc-kieu-moi-AD.jpg")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-23.jpg?v=1586766861037")
+        images.append("https://quyennguyen.vn/wp-content/uploads/2020/06/mau-ao-dai-an-hoi-dep-2020-1.jpg")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-8237.jpg?v=1593587945767")
+        images.append("https://cf.shopee.vn/file/0254aea1b7d9cc4c95435563a4e5fa89")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-30.jpg?v=1586766112957")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-3495.jpg?v=1593490441000")
+        images.append("https://aodaitrunghieu.com/wp-content/uploads/2019/07/Vai-ao-dai-hoa-3D-tren-nguc-kieu-moi-AD.jpg")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-23.jpg?v=1586766861037")
+        images.append("https://quyennguyen.vn/wp-content/uploads/2020/06/mau-ao-dai-an-hoi-dep-2020-1.jpg")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-8237.jpg?v=1593587945767")
+        images.append("https://cf.shopee.vn/file/0254aea1b7d9cc4c95435563a4e5fa89")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-30.jpg?v=1586766112957")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-3495.jpg?v=1593490441000")
+        images.append("https://aodaitrunghieu.com/wp-content/uploads/2019/07/Vai-ao-dai-hoa-3D-tren-nguc-kieu-moi-AD.jpg")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-23.jpg?v=1586766861037")
+        images.append("https://quyennguyen.vn/wp-content/uploads/2020/06/mau-ao-dai-an-hoi-dep-2020-1.jpg")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-8237.jpg?v=1593587945767")
+        images.append("https://cf.shopee.vn/file/0254aea1b7d9cc4c95435563a4e5fa89")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-30.jpg?v=1586766112957")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-3495.jpg?v=1593490441000")
+        images.append("https://aodaitrunghieu.com/wp-content/uploads/2019/07/Vai-ao-dai-hoa-3D-tren-nguc-kieu-moi-AD.jpg")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-23.jpg?v=1586766861037")
+        images.append("https://quyennguyen.vn/wp-content/uploads/2020/06/mau-ao-dai-an-hoi-dep-2020-1.jpg")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-8237.jpg?v=1593587945767")
+        images.append("https://cf.shopee.vn/file/0254aea1b7d9cc4c95435563a4e5fa89")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-30.jpg?v=1586766112957")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-3495.jpg?v=1593490441000")
+        images.append("https://aodaitrunghieu.com/wp-content/uploads/2019/07/Vai-ao-dai-hoa-3D-tren-nguc-kieu-moi-AD.jpg")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-23.jpg?v=1586766861037")
+        images.append("https://quyennguyen.vn/wp-content/uploads/2020/06/mau-ao-dai-an-hoi-dep-2020-1.jpg")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-8237.jpg?v=1593587945767")
+        images.append("https://cf.shopee.vn/file/0254aea1b7d9cc4c95435563a4e5fa89")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-30.jpg?v=1586766112957")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-3495.jpg?v=1593490441000")
+        images.append("https://aodaitrunghieu.com/wp-content/uploads/2019/07/Vai-ao-dai-hoa-3D-tren-nguc-kieu-moi-AD.jpg")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-23.jpg?v=1586766861037")
+        images.append("https://quyennguyen.vn/wp-content/uploads/2020/06/mau-ao-dai-an-hoi-dep-2020-1.jpg")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-8237.jpg?v=1593587945767")
+        images.append("https://cf.shopee.vn/file/0254aea1b7d9cc4c95435563a4e5fa89")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-30.jpg?v=1586766112957")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-3495.jpg?v=1593490441000")
+        images.append("https://aodaitrunghieu.com/wp-content/uploads/2019/07/Vai-ao-dai-hoa-3D-tren-nguc-kieu-moi-AD.jpg")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-23.jpg?v=1586766861037")
+        images.append("https://quyennguyen.vn/wp-content/uploads/2020/06/mau-ao-dai-an-hoi-dep-2020-1.jpg")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-8237.jpg?v=1593587945767")
+        images.append("https://cf.shopee.vn/file/0254aea1b7d9cc4c95435563a4e5fa89")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-30.jpg?v=1586766112957")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-3495.jpg?v=1593490441000")
+        images.append("https://aodaitrunghieu.com/wp-content/uploads/2019/07/Vai-ao-dai-hoa-3D-tren-nguc-kieu-moi-AD.jpg")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-23.jpg?v=1586766861037")
+        images.append("https://quyennguyen.vn/wp-content/uploads/2020/06/mau-ao-dai-an-hoi-dep-2020-1.jpg")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-8237.jpg?v=1593587945767")
+        images.append("https://cf.shopee.vn/file/0254aea1b7d9cc4c95435563a4e5fa89")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-30.jpg?v=1586766112957")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-3495.jpg?v=1593490441000")
+        images.append("https://aodaitrunghieu.com/wp-content/uploads/2019/07/Vai-ao-dai-hoa-3D-tren-nguc-kieu-moi-AD.jpg")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-23.jpg?v=1586766861037")
+        images.append("https://quyennguyen.vn/wp-content/uploads/2020/06/mau-ao-dai-an-hoi-dep-2020-1.jpg")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-8237.jpg?v=1593587945767")
+        images.append("https://cf.shopee.vn/file/0254aea1b7d9cc4c95435563a4e5fa89")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-30.jpg?v=1586766112957")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-3495.jpg?v=1593490441000")
+        images.append("https://aodaitrunghieu.com/wp-content/uploads/2019/07/Vai-ao-dai-hoa-3D-tren-nguc-kieu-moi-AD.jpg")
+        images.append("https://bizweb.dktcdn.net/100/236/196/products/ao-dai-in-3d-23.jpg?v=1586766861037")
+        images.append("https://quyennguyen.vn/wp-content/uploads/2020/06/mau-ao-dai-an-hoi-dep-2020-1.jpg")
+        images.append("https://bizweb.dktcdn.net/100/386/329/products/dsc-8237.jpg?v=1593587945767")
     }
 }
 
@@ -27,13 +109,32 @@ extension ViewController: UICollectionViewDelegate {
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 30
+        return images.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier, for: indexPath)
+        (cell as! PhotoCollectionViewCell).url = images[indexPath.row]
+        return cell
     }
-    
-    
 }
 
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(
+            width: view.frame.width / 3 - 2,
+            height: view.frame.width / 3 - 2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
+    }
+}
